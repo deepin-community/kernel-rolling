d5c0db6964c09c590630f44d0c08f1716ac82b75
leoliu-oc <leoliu-oc@zhaoxin.com>
2024-06-14 17:18:37 +0800

    x86/delay: add support for Zhaoxin ZXPAUSE instruction
    zhaoxin inclusion
    category: feature
    
    -------------------
    
    ZXPAUSE instructs the processor to enter an implementation-dependent
    optimized state. The instruction execution wakes up when the time-stamp
    counter reaches or exceeds the implicit EDX:EAX 64-bit input value.
    The instruction execution also wakes up due to the expiration of
    the operating system time-limit or by an external interrupt.
    
    ZXPAUSE is available on processors with X86_FEATURE_ZXPAUSE.
    ZXPAUSE allows the processor to enter a light-weight power/performance
    optimized state (C0.1 state) for a period specified by the instruction
    or until the system time limit.
    
    MSR_ZX_PAUSE_CONTROL MSR register allows the OS to enable/disable C0.2 on
    the processor and to set the maximum time the processor can reside in C0.1
    or C0.2. By default C0.2 is disabled.
    
    A sysfs interface to adjust the time and the C0.2 enablement is provided in
    a follow up change.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>
    (cherry picked from commit 059f4703d1db2482adf8e9d31c24c67e096aae7e)
    
    Conflicts:
    	tools/arch/x86/include/asm/cpufeatures.h

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -13,7 +13,7 @@
 /*
  * Defines x86 CPU feature bits
  */
-#define NCAPINTS			22	   /* N 32-bit words worth of info */
+#define NCAPINTS			23	   /* N 32-bit words worth of info */
 #define NBUGINTS			2	   /* N 32-bit bug flags */
 
 /*
@@ -499,6 +499,9 @@
 #define X86_FEATURE_AMD_FAST_CPPC	(21*32 + 5) /* Fast CPPC */
 #define X86_FEATURE_AMD_HETEROGENEOUS_CORES (21*32 + 6) /* Heterogeneous Core Topology */
 #define X86_FEATURE_AMD_WORKLOAD_CLASS	(21*32 + 7) /* Workload Classification */
+
+/* VIA/Cyrix/Centaur-defined CPU features, CPUID level 0xC0000006, word 22 */
+#define X86_FEATURE_ZXPAUSE            (22*32+ 0) /* ZHAOXIN ZXPAUSE */
 
 /*
  * BUG word(s)
