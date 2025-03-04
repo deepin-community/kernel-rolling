ea8840f3e5eef8827d099442d679c97098902f8e
Shu Hang <shuh2023@zgclab.edu.cn>
2025-03-04 14:57:26 +0800

    HAOC: Add kernel command line support for x86 IEE.
    community inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBQKOW
    
    -----------------------------------
    
    Use kernel command line haoc to control if HAOC should be enabled.
    eg: haoc=on to enable haoc protection.
    
    Signed-off-by: Shu Hang <shuh2023@zgclab.edu.cn>
    Signed-off-by: Hu Bing <hubing2023@zgclab.edu.cn>
    Signed-off-by: Liu Zhehui <liuzhh@zgclab.edu.cn>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -1374,7 +1374,8 @@ void __init mem_init(void)
 	 * The upper region becomes the IEE linear mapping area.
 	 * Note that the IEE mapping region is mapped with read-only permissions.
 	 */
-	iee_init();
+	if (haoc_enabled)
+		iee_init();
 	#endif
 	preallocate_vmalloc_pages();
 }
