1a8267450cc12b9f335fb8c39562b74e6d036bad
Wentao Guan <guanwentao@uniontech.com>
2025-05-28 23:03:34 +0800

    Revert "tty: Add Chinese fonts support in console"
    It does not works ,and it cause ci always failed.
    
    This reverts commit f0d4e981f678896805ee76740460605af382e6a0.

--- include/linux/font.h
+++ include/linux/font.h
@@ -35,7 +35,6 @@ struct font_desc {
 #define FONT6x10_IDX	10
 #define TER16x32_IDX	11
 #define FONT6x8_IDX	12
-#define UTF8x16_IDX	13
 
 extern const struct font_desc	font_vga_8x8,
 			font_vga_8x16,
@@ -49,8 +48,7 @@ extern const struct font_desc	font_vga_8x8,
 			font_mini_4x6,
 			font_6x10,
 			font_ter_16x32,
-			font_6x8,
-			font_utf8_8x16;
+			font_6x8;
 
 /* Find a font with a specific name */
 
