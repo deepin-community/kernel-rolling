deepin-community/kernel

sw64: adapt to the upstream changes in early_init_dt_(scan/verify)
Fix it according to commit b2473a359763 ("of/fdt: add dt_phys arg to
early_init_dt_scan and early_init_dt_verify").

Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/kernel/setup.c
@@ -483,7 +483,7 @@ static void __init setup_firmware_fdt(void)
 	memblock_reserve(__boot_pa(dt_virt), fdt_totalsize(dt_virt));
 
 	if (!arch_dtb_verify(dt_virt, true) ||
-			!early_init_dt_scan(dt_virt)) {
+			!early_init_dt_scan(dt_virt, __pa(dt_virt))) {
 		pr_crit("Invalid DTB(from firmware) at virtual address 0x%lx\n",
 				(unsigned long)dt_virt);
 
@@ -578,7 +578,7 @@ static void __init setup_builtin_fdt(void)
 
 	dt_virt = (void *)__dtb_start;
 	if (!arch_dtb_verify(dt_virt, false) ||
-			!early_init_dt_verify(dt_virt)) {
+			!early_init_dt_verify(dt_virt, __pa(dt_virt))) {
 		pr_crit("Invalid DTB(built-in) at virtual address 0x%lx\n",
 				(unsigned long)dt_virt);
 		while (true)

deepin-community/kernel-rolling

Fix it according to upstream commit b2473a359763 ("of/fdt: add dt_phys
arg to early_init_dt_scan and early_init_dt_verify").
Reviewed-by: He Sheng <hesheng@wxiat.com>
(cherry picked from commit e985389a88eedff255b345b8c74ca22cd91cebd8)
(cherry picked from commit 6d51bf89d0fefe4edcc228f378862b7508f1aeb5)
(cherry picked from commit 22e810e77e6659f53b068ec5680ccd419f4fecbb)

arch/sw_64/kernel/setup.c
@@ -483,7 +483,7 @@ static void __init setup_firmware_fdt(void)
 	memblock_reserve(__boot_pa(dt_virt), fdt_totalsize(dt_virt));
 
 	if (!arch_dtb_verify(dt_virt, true) ||
-			!early_init_dt_scan(dt_virt)) {
+			!early_init_dt_scan(dt_virt, __boot_pa(dt_virt))) {
 		pr_crit("Invalid DTB(from firmware) at virtual address 0x%lx\n",
 				(unsigned long)dt_virt);
 
@@ -578,7 +578,7 @@ static void __init setup_builtin_fdt(void)
 
 	dt_virt = (void *)__dtb_start;
 	if (!arch_dtb_verify(dt_virt, false) ||
-			!early_init_dt_verify(dt_virt)) {
+			!early_init_dt_verify(dt_virt, __boot_pa(dt_virt))) {
 		pr_crit("Invalid DTB(built-in) at virtual address 0x%lx\n",
 				(unsigned long)dt_virt);
 		while (true)