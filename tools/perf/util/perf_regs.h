deepin-community/kernel

perf jitdump: Add SW64 support
Signed-off-by: He Chuyue <hechuyue@wxiat.com>

tools/perf/util/perf_regs.h
@@ -58,6 +58,9 @@ uint64_t __perf_reg_sp_riscv(void);
 const char *__perf_reg_name_s390(int id);
 uint64_t __perf_reg_ip_s390(void);
 uint64_t __perf_reg_sp_s390(void);
+const char *__perf_reg_name_sw_64(int id);
+uint64_t __perf_reg_ip_sw_64(void);
+uint64_t __perf_reg_sp_sw_64(void);
 const char *__perf_reg_name_x86(int id);
 uint64_t __perf_reg_ip_x86(void);
 uint64_t __perf_reg_sp_x86(void);

deepin-community/kernel-rolling

Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>
(cherry picked from commit 51ddc7ab16d2dc2681e0f190ecfbb8c45600a81d)
(cherry picked from commit 47580ccb3b07ec7c0f7a9a6b606531097b1cf9cd)
(cherry picked from commit e83bf8e8680cf2bcf572eec9581639a4518e363b)

tools/perf/util/perf_regs.h
@@ -58,6 +58,9 @@ uint64_t __perf_reg_sp_riscv(void);
 const char *__perf_reg_name_s390(int id);
 uint64_t __perf_reg_ip_s390(void);
 uint64_t __perf_reg_sp_s390(void);
+const char *__perf_reg_name_sw64(int id);
+uint64_t __perf_reg_ip_sw64(void);
+uint64_t __perf_reg_sp_sw64(void);
 const char *__perf_reg_name_x86(int id);
 uint64_t __perf_reg_ip_x86(void);
 uint64_t __perf_reg_sp_x86(void);